aa528bf6fbacf0a251bf0eb63ae97986a0ddba6b
Ajeet <ajeetmdl23@gmail.com>
2023-05-05 00:42:30 -0400

    updated

---
+++ README.md
@@ -0,0 +1,3 @@
+### To launch iSEE instance: a browsable data portal:
+https://mybinder.org/v2/gh/ajeetmandal/iSEE_posted_on_JNS.git/main?urlpath=shiny/shinyR_HBCC_FigShare/
+https://mybinder.org/v2/gh/ajeetmandal/iSEE_posted_on_JNS.git/HEAD
---
+++ install.R
@@ -0,0 +1,18 @@
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("iSEE")
+install.packages("RColorBrewer")
+
+#BiocManager::install("SingleCellExperiment") # Dont need this as "iSEE" has this
+#BiocManager::install("scMerge")
+
+# install.packages("shiny")  # Dont need this as "iSEE" has this
+
+
+# install.packages("tidyverse")
+# install.packages("rmarkdown")
+# install.packages("httr")
+# install.packages("shinydashboard")
+# install.packages('leaflet')
+
